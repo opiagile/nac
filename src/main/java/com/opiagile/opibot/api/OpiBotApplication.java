c1424fafde0a4565f99174951734f2f2753a0754
Francisco Araujo <francisco@opiagile.com>
2017-11-26 17:53:13 -0200

    Commit inicial dos arquivos do projeto

---
+++ src/main/java/com/opiagile/opibot/api/OpiBotApplication.java
@@ -0,0 +1,12 @@
+package com.opiagile.opibot.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpiBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpiBotApplication.class, args);
+	}
+}
---
+++ src/test/java/com/opiagile/opibot/api/OpiBotApplicationTests.java
@@ -0,0 +1,16 @@
+package com.opiagile.opibot.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OpiBotApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
